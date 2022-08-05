f253bc66d37a0a391894237387315ce114d06772
romanprotoliuk <romanprotoliuk@gmail.com>
2022-08-05 11:24:50 -0700

    ground work

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+const App = () => {
+	
+	console.log(process.env.REACT_APP_SUPABASE_URL)
+	return (
+		<div>Hello world</div>
+	);
+}
+
+export default App;
---
+++ src/utils/supabase.js
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const url = process.env.REACT_APP_SUPABASE_URL;
+
+const key = process.env.REACT_APP_SUPABASE_KEY;
+
+export const supabase = createClient(url, key);
