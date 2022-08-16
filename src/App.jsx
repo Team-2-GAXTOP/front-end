019791a772c8f1d84f789d3d9ce3972899d54c68
romanprotoliuk <romanprotoliuk@gmail.com>
2022-08-15 22:19:12 -0700

    wip onboarding

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Landing from './pages/Landing/Landing';
 import Login from './pages/Login/Login';
 import Signup from './pages/Signup/Signup';
 import Navbar from './components/Navbar/Navbar';
+import Onboarding from './pages/Onboarding/Onboarding';
 
 const App = () => {
 	
@@ -37,6 +38,12 @@ const App = () => {
 				path='/signup'
 				element={
 					<Signup />
+				}
+				/>
+			<Route
+				path='/onboarding'
+				element={
+					<Onboarding />
 				}
 			/>
 		</Routes>
--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,7 +1,10 @@
+import Searchbar from "../../components/Searchbar/Searchbar"
+
 const Landing = () => {
   return (
     <>
       <h1>Welcome to our landing page</h1>
+      <Searchbar />
     </>
   )
 }
